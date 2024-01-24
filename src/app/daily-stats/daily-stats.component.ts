3ef55e4a74e4338ceef7d92ae27054e317364c0d
sana mushtaq <sana.mushtaq@hotmail.com>
2024-01-24 20:12:04 +0500

    update

--- src/app/daily-stats/daily-stats.component.ts
+++ src/app/daily-stats/daily-stats.component.ts
@@ -1,43 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StatsService } from 'src/service/stats.service';
-import { APIResponse } from 'src/utils/app-enum'
+import { APIResponse } from 'src/utils/app-enum';
 
 @Component({
   selector: 'app-daily-stats',
   templateUrl: './daily-stats.component.html',
   styleUrls: ['./daily-stats.component.css']
 })
-export class DailyStatsComponent implements OnInit {
+export class DailyStatsComponent implements OnInit, OnDestroy {
 
-  data: any = {};
+  data: any = {recordCount:0, upcomingCount: 0, completedCount: 0, cancelledCount:0};
+  private intervalId: any;
 
   constructor(private statService: StatsService) { }
 
   ngOnInit(): void {
+    // Initial call to fetch data
+    this.fetchData();
 
+    // Set up a recurring call every 10 seconds
+    this.intervalId = setInterval(() => {
+      this.fetchData();
+    }, 10000);
+  }
+
+  ngOnDestroy(): void {
+    // Clear the interval to prevent memory leaks
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private fetchData() {
     let params = {
       "data": "data"
     };
-    //now we will get a list of categories from the backend
+
     this.statService.dailyStats(params).subscribe({
-  
-      next : ( res : any ) => {
-
-        //in case of success the api returns 0 as a status code
-        if( res.status === APIResponse.Success) {
-
+      next: (res: any) => {
+        if (res.status === APIResponse.Success) {
           this.data = res.data;
-
-        } 
+        }
       },
-      error: ( err: any ) => {
-
-        console.log(err)
-
+      error: (err: any) => {
+        console.log(err);
       }
-  
-    })
-        
+    });
   }
-
 }
